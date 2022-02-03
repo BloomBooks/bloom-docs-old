d37ff90bf7c383f5e15689e3a12b20f4d3c62eed
Hatton <hattonjohn@gmail.com>
2022-02-02 18:17:10 -0700

    Clear out most boilerplate

---
+++ docs/overview.md
@@ -0,0 +1,11 @@
+---
+sidebar_position: 1
+sidebar_label: 'Overview'
+---
+# Here is the problem Bloom addresses:
+When working in local-language literacy projects, we end up settling for what is practical instead of what is actually needed. The hope is often that a primer and a handful of books will be enough to open up the joy and power of reading in minority language communities. But realistically, we know that it takes stacks of titles to make reading worthwhile and give the practice needed to develop true literacy. The problem has been who will make all those books? How will we find the time? Where will we get the content? So we settle for the possible and hope for the best.
+
+# Build Local Capacity
+Bloom dramatically lowers the bar so that many more people can get involved in building a large collection of local language books. We designed Bloom with new computer users in mind, and it has special features to guide them in simple bookmaking, so people need far less training than alternatives such as Word, Publisher, or In Design.
+
+With Bloom, you can go beyond simple paper books where appropriate. You can easily create talking books, comic books, books for the blind, sign language books, and books with interactive activities. You can then publish these as e-books, books for websites, and stand-alone mobile apps.
---
+++ docusaurus.config.js
@@ -0,0 +1,64 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Bloom Docs',
+  tagline: "Let's grow a library",
+  url: 'https://docs.bloomlibrary.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'BloomBooks',
+  projectName: 'bloom-docs', 
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          routeBasePath: '/', // Serve the docs at the site's root
+          sidebarPath: require.resolve('./sidebars.js'),
+          editUrl: 'https://github.com/BloomBooks/bloom-docs',
+        },
+        blog:false,
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'Bloom Docs',
+        logo: {
+          alt: 'My Site Logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'overview',
+            position: 'left',
+            label: 'Docs',
+          },
+        ],
+      },
+    
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+
+    }),
+};
+
+module.exports = config;
